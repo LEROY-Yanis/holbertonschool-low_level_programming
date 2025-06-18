6bd7583df81c34ea989f52df580ee869e49ba1bc
LEROY-Yanis <11479@holbertonstudents.com>
2025-06-18 14:56:17 +0000

    print alphabet sans e,q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int i = 'a';
+
+while(i <= 'z')
+{
+if (i != 'e' && i != 'q')
+{
+putchar(i);
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
