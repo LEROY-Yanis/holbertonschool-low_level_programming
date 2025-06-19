5bec2c2d51bd9b03fbed97358e2cb8dac338f072
LEROY-Yanis <11479@holbertonstudents.com>
2025-06-19 10:19:21 +0000

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+int i = 0;
+for (; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
