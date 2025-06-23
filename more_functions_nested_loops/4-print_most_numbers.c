a36ce46266c10f33ccfbc27c91b2ba67d15f759d
LEROY-Yanis <11479@holbertonstudents.com>
2025-06-23 14:48:12 +0000

    prit most numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,16 @@
 #include "main.h"
-
 /**
-*print_numbers - Prints the numbers from 0 to 9, followed by a new line
+* print_most_numbers - Prints the numbers from 0 to 9
 *
-*Description: This function uses _putchar to print each number
-*
-*and can only use _putchar twice in the code
+* and can only use _putchar twice in the code
 */
 
 void print_most_numbers(void)
 {
 int i;
 for (i = '0'; i <= '9'; i++)
-{
 if (i != '2' && i != '4')
+{
 _putchar(i);
 }
 _putchar('\n');
