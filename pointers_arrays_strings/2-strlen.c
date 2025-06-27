31591862c848be720de0bbc339df48d7e8756e7d
LEROY-Yanis <11479@holbertonstudents.com>
2025-06-27 15:07:47 +0000

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int len;
+	for (len = 0; s[len] != '\0'; )
+		len++;
+	return (len);
+}
+
