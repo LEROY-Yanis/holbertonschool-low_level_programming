4adc37352b04ac9b67349132e618cd3c01be0f6b
LEROY-Yanis <11479@holbertonstudents.com>
2025-07-02 16:40:30 +0200

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -6,28 +6,28 @@
  * @s: pointer initial segment
  * @accept: pointer substring
  *
- * return number of bytes
+ * Return: number of bytes
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-    int i;
+	int i;
 
-    for (i = 0; s[i] !='\0'; i++)
-    {
-        int j;
-        int found = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		int j;
+		int found = 0;
 
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[i] == accept[j])
-            {
-                found = 1;
-                break;
-            }
-        }
-        if (!found)
-            break;
-    }
-    return (i);
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (!found)
+			break;
+	}
+	return (i);
 }
