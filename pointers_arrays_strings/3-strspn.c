7119b0aac1fb7c12985436e1d88498fa6607a76b
LEROY-Yanis <11479@holbertonstudents.com>
2025-07-02 16:37:58 +0200

    strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - calculates the length of the initial segment
+ * @s: pointer initial segment
+ * @accept: pointer substring
+ *
+ * return number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+    int i;
+
+    for (i = 0; s[i] !='\0'; i++)
+    {
+        int j;
+        int found = 0;
+
+        for (j = 0; accept[j] != '\0'; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                found = 1;
+                break;
+            }
+        }
+        if (!found)
+            break;
+    }
+    return (i);
+}
