899b707de38b798b88f43f5019e507d34894a3c0
LEROY-Yanis <11479@holbertonstudents.com>
2025-07-02 13:59:09 +0200

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @c: charcter
+ * @s: the string
+ * Return: c succes NULL: if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+
+{
+    int i;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == c)
+        return (i + s);
+    }
+    return (NULL);
+}
