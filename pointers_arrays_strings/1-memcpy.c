2d13c8f1861fabdc33248ca89c075707c0069de7
LEROY-Yanis <11479@holbertonstudents.com>
2025-07-02 09:53:58 +0200

    Refactor simple_print_buffer and _memcpy functions for improved readability and functionality

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,21 @@
-#include(Main.h)
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - returns a pointer to dest
+ * @n: number
+ * @src: pointer source
+ * @dest: pointer destination
+ *
+ * Return: dest (succes)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
