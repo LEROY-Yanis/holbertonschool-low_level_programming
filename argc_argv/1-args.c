80f3b482dec04721d333c15692242d3ec505066d
LEROY-Yanis <11479@holbertonstudents.com>
2025-07-03 17:40:20 +0200

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: Arguments count
+ * @argv: Array of args
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+		return (0);
+}
