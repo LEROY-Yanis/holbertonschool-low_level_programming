ffa36b0a7c2c26aff8a247ab9a209b8aaf0af12a
LEROY-Yanis <11479@holbertonstudents.com>
2025-07-08 10:45:07 +0200

    alloc grind

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid -  returns a pointer to a two dimensional array of integers
+ * @width: vertical
+ * @height: hoorizental
+ * Return: grid or null
+ */
+
+int **alloc_grid(int width, int height)
+{
+    int i, j;
+    int **grid;
+
+    if (width <= 0 && height <= 0)
+        return (NULL);
+
+    grid = malloc(sizeof(int *) * height);
+    if (grid == NULL)
+        return (NULL);
+
+    for (i = 0; i < height; i++) 
+	{
+        grid[i] = malloc(sizeof(int) * width);
+        if (grid[i] == NULL) 
+		{
+            while (i-- > 0)
+                free(grid[i]);
+            free(grid);
+            return (NULL);
+        }
+        for (j = 0; j < width; j++)
+            grid[i][j] = 0;
+    }
+    return (grid);
+}
