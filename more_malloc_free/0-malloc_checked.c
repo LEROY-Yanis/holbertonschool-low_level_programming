13852c9f0e3ec82ef0d2904f46bda04ace389bd8
LEROY-Yanis <11479@holbertonstudents.com>
2025-07-09 11:52:24 +0200

    lalloc checked

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * 
+ */
+
+void *malloc_checked(unsigned int b)
+{
+    void *m = malloc(b);
+    if (m == NULL)
+        exit(98);
+    return (m);
+}
