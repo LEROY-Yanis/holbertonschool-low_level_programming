400eca584025c4b686adce8c06373cf73d34d6f6
LEROY-Yanis <11479@holbertonstudents.com>
2025-07-19 14:01:03 +0200

    sum them all

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - a function that returns the sum of all its parameters.
+ * @n: the number of parameters.
+ * @...: the numbers of parameters stored.
+ * Return: the sum of all its parameters, 0 if n == 0.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list arg;
+	unsigned int i = 0;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(arg, n);
+
+	while (i < n)
+	{
+		sum = sum + va_arg(arg, int);
+		i++;
+	}
+
+	va_end(arg);
+
+	return (sum);
+}
