b115841e9fc693d8a892abb2abb15788a31bc0b3
LEROY-Yanis <11479@holbertonstudents.com>
2025-07-29 10:46:49 +0200

    Add print list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to the head of the list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		_putchar('[');
+		if (h->str == NULL)
+		{
+			_putchar('0');
+			_putchar(']');
+			_putchar(' ');
+			_putchar('(');
+			_putchar('n');
+			_putchar('i');
+			_putchar('l');
+			_putchar(')');
+			_putchar('\n');
+		}
+		else
+		{
+			/* Print length */
+			unsigned int len = h->len, div = 1;
+			while (len / div >= 10)
+				div *= 10;
+			while (div)
+			{
+				_putchar('0' + len / div);
+				len %= div;
+				div /= 10;
+			}
+			_putchar(']');
+			_putchar(' ');
+			/* Print string */
+			for (unsigned int i = 0; h->str[i] != '\0'; i++)
+				_putchar(h->str[i]);
+			_putchar('\n');
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
