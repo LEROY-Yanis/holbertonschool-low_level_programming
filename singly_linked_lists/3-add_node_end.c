ffc2740d2b49d920f71bcc212e646fdf82106bcb
LEROY-Yanis <11479@holbertonstudents.com>
2025-07-30 09:57:26 +0200

    Update add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -31,10 +31,11 @@ list_t *add_node_end(list_t **head, const char *str)
         *head = new;
     
     else
-        temp = *head;
+    {
+		temp = *head;
         while (temp->next)
             temp = temp->next;
         temp->next = new;
-
+	}
 	return (new);
 }
