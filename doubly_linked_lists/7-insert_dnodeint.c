a0f713df196f9a5064c6d309b9af84470fed2cfe
LEROY-Yanis <11479@holbertonstudents.com>
2025-08-02 20:42:42 +0200

    fix the probleme and sup the space

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,15 +15,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
-
 	if (idx == 0)
 	{
 		new_node->next = *h;
@@ -32,23 +29,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
-
 	current = *h;
 	for (i = 0; current != NULL && i < idx - 1; i++)
 		current = current->next;
-
 	if (current == NULL && i < idx - 1)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	new_node->next = current->next;
 	if (current->next != NULL)
 		current->next->prev = new_node;
-	
 	current->next = new_node;
 	new_node->prev = current;
-
 	return (new_node);
 }
